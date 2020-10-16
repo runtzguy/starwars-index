a42439b0f6d15850f58c2bd26906103f9ada4cea
Victor Liu <>
2020-10-15 17:30:52 -0700

    Added Spinner animation to loading component and centered it on the screen

--- client/src/components/LoadingSpinner.js
+++ client/src/components/LoadingSpinner.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Spinner} from 'react-bootstrap'
+
+import "../css/spinnerStyle.css"
 
 export default function LoadingSpinner() {
    
-
     return (
-        <div>
-            Loading... 
+        <div className="spinner-wrapper">
+            <Spinner animation="border" size="sm"></Spinner>
+            <span>Loading...</span>
         </div>
     )
 }
